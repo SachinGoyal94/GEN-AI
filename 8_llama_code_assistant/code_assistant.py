71a7aef69aee59c8866a225a08287921e25cc8df
Sachin Goyal <sachingoyal9274@gmail.com>
2025-07-22 00:29:17 +0530

    Created Code Assistant

---
+++ 8_llama_code_assistant/code_assistant.py
@@ -0,0 +1,39 @@
+#now already created a custom model now we can integrate it with langchain and gradio interface
+import gradio as gr,json,requests
+
+url="http://localhost:11434/api/generate"
+
+headers={
+
+    'Content-Type':'application/json'
+}
+
+history=[]
+
+def generate_response(prompt):
+    history.append(prompt)
+    final_prompt="\n".join(history)
+
+    data={
+        "model":"codeguru",
+        "prompt":final_prompt,
+        "stream":False
+    }
+
+    response=requests.post(url,headers=headers,data=json.dumps(data))
+
+    if response.status_code==200:
+        response=response.text
+        data=json.loads(response)
+        actual_response=data['response']
+        return actual_response
+    else:
+        print("error:",response.text)
+
+
+interface=gr.Interface(
+    fn=generate_response,
+    inputs=gr.Textbox(lines=4,placeholder="Enter your Prompt"),
+    outputs="text"
+)
+interface.launch()
