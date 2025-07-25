be4fb1f752f3e57113157089b2ebfbbc98be781a
Sachin Goyal <sachingoyal9274@gmail.com>
2025-07-25 17:57:15 +0530

    minor updates

--- check.py
+++ check.py
@@ -1,15 +1,6 @@
-import streamlit as st
-
-# Title
-st.set_page_config(page_title="Markdown Viewer", layout="wide")
-st.title("📄 Markdown Viewer Web App")
-
-# File uploader
-uploaded_file = st.file_uploader("Upload a .md file", type=["md", "markdown"])
-
-# If file is uploaded, display it
-if uploaded_file is not None:
-    content = uploaded_file.read().decode("utf-8")
-    st.markdown(content, unsafe_allow_html=True)
-else:
-    st.info("👈 Upload a Markdown (.md) file to view its content here.")
+import sqlite3
+print("SQLite version:", sqlite3.sqlite_version)
+print("SQLite version info:", sqlite3.sqlite_version_info)
+print("Check result:", sqlite3.sqlite_version_info < (3, 35, 0))
+print("is_client value:", is_client)
+print("IN_COLAB value:", IN_COLAB)
