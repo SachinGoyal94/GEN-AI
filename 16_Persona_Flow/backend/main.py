7e0e5d5923b13bdaa727ead7ec3e346531217a81
Sachin Goyal <sachingoyal9274@gmail.com>
2025-10-14 13:57:26 +0530

    helper

--- 16_Persona_Flow/backend/main.py
+++ 16_Persona_Flow/backend/main.py
@@ -252,6 +252,5 @@ def get_history(user_id: int, persona_id: int, db: Session = Depends(get_db)):
 if __name__ == "__main__":
     import uvicorn
     import os
-
-    port = int(os.environ.get("PORT", 10000))  
+    port = int(os.environ.get("PORT", 8888))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
