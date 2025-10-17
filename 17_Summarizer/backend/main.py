68b4f6cf308b5ccbb238e36befe0407b5ab9513a
Sachin Goyal <sachingoyal9274@gmail.com>
2025-10-17 16:30:26 +0530

    acc to render

--- 17_Summarizer/backend/main.py
+++ 17_Summarizer/backend/main.py
@@ -229,5 +229,5 @@ async def generate_blog_from_web(url: str = Query(...),summary: str = Query(None
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
