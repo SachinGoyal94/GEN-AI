d3b206580bd75fb1d20ffa47e2710d0b5a28d363
Sachin Goyal <sachingoyal9274@gmail.com>
2025-10-20 17:50:12 +0530

    updated
    req and agents imports updated

--- 19_DB_Chat/main.py
+++ 19_DB_Chat/main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from sqlalchemy import create_engine
-from langchain_community.agent_toolkits.sql.base  import create_sql_agent
+from langchain_community.agent_toolkits import create_sql_agent
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain.agents.agent_types import AgentType
