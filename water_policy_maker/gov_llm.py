1c6fcb5794c5b9bab5c92b74b79e156e0d021863
Sachin Goyal <sachingoyal9274@gmail.com>
2025-10-24 18:57:48 +0530

    Delete water_policy_maker directory

--- water_policy_maker/gov_llm.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from crewai import LLM
-load_dotenv()
-gemini_key = os.getenv("GEMINI_KEY")
-gemini_llm = LLM(
-    model="gemini/gemini-2.0-flash",
-    api_key=gemini_key
-)
